491bbf1e1f898b923d1f183b8b92c49106a06e35
Damian Joniec <damianjoniec@damians-mbp.home>
2019-08-22 21:09:34 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          *******************
+          *******************
+          my second commit
+          *******************
+          *******************
         </p>
         <a
           className="App-link"
